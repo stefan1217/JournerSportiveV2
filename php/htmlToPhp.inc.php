b1fb35bcfa992dc1f270f6148071de76384be74d
stefan1217 <stefan.nklc@eduge.ch>
2021-09-01 11:04:16 +0200

    FormulaireJournerSport

---
+++ php/htmlToPhp.inc.php
@@ -0,0 +1,9 @@
+<?php
+function afficherSelectActivites($name){
+echo "<select id=activite>
+<option value=accrobranche>Accrobranche</option>
+<option value=velo>Vélo</option>
+<option value=football>Football</option>
+</select>";
+}
+?>
